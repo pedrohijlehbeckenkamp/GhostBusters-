b9df6b311a4a46e37eb8de1af62da0f2313c6ae7
Fabricio Durand <fabricio.silva@viaflow.com.br>
2019-07-16 14:44:03 -0300

    Backup - Tela login

--- GhostBusters_2/GhostBusters_Forms/View/Adm/ConfigAdm.Designer.cs
+++ GhostBusters_2/GhostBusters_Forms/View/Adm/ConfigAdm.Designer.cs
@@ -28,70 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.btnVisualizarStatus = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
-            this.btnPerfil = new System.Windows.Forms.Button();
-            this.btnCadastrar = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgVisualizar = new System.Windows.Forms.DataGridView();
-            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnVisualizarCategoria = new System.Windows.Forms.Button();
-            this.btnVisualizarPerfil = new System.Windows.Forms.Button();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCategoria = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgVisualizar)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnVisualizarStatus
-            // 
-            this.btnVisualizarStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnVisualizarStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVisualizarStatus.ForeColor = System.Drawing.Color.White;
-            this.btnVisualizarStatus.Location = new System.Drawing.Point(32, 27);
-            this.btnVisualizarStatus.Margin = new System.Windows.Forms.Padding(2);
-            this.btnVisualizarStatus.Name = "btnVisualizarStatus";
-            this.btnVisualizarStatus.Size = new System.Drawing.Size(116, 39);
-            this.btnVisualizarStatus.TabIndex = 37;
-            this.btnVisualizarStatus.Text = "Visualizar status";
-            this.btnVisualizarStatus.UseVisualStyleBackColor = false;
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditar.ForeColor = System.Drawing.Color.White;
-            this.btnEditar.Location = new System.Drawing.Point(475, 86);
-            this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(116, 39);
-            this.btnEditar.TabIndex = 38;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = false;
-            // 
-            // btnPerfil
-            // 
-            this.btnPerfil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPerfil.ForeColor = System.Drawing.Color.White;
-            this.btnPerfil.Location = new System.Drawing.Point(475, 151);
-            this.btnPerfil.Margin = new System.Windows.Forms.Padding(2);
-            this.btnPerfil.Name = "btnPerfil";
-            this.btnPerfil.Size = new System.Drawing.Size(116, 39);
-            this.btnPerfil.TabIndex = 39;
-            this.btnPerfil.Text = "Perfil";
-            this.btnPerfil.UseVisualStyleBackColor = false;
-            // 
-            // btnCadastrar
-            // 
-            this.btnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
-            this.btnCadastrar.Location = new System.Drawing.Point(475, 27);
-            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(2);
-            this.btnCadastrar.Name = "btnCadastrar";
-            this.btnCadastrar.Size = new System.Drawing.Size(116, 39);
-            this.btnCadastrar.TabIndex = 40;
-            this.btnCadastrar.Text = "Cadastrar";
-            this.btnCadastrar.UseVisualStyleBackColor = false;
             // 
             // dgVisualizar
             // 
@@ -99,92 +46,129 @@
             this.dgVisualizar.AllowUserToResizeColumns = false;
             this.dgVisualizar.AllowUserToResizeRows = false;
             this.dgVisualizar.BackgroundColor = System.Drawing.Color.DarkOrange;
-            this.dgVisualizar.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgVisualizar.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
             this.dgVisualizar.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgVisualizar.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgVisualizar.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dgVisualizar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgVisualizar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Nome,
-            this.Codigo});
-            this.dgVisualizar.EnableHeadersVisualStyles = false;
+            this.Codigo,
+            this.Nome});
             this.dgVisualizar.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dgVisualizar.Location = new System.Drawing.Point(191, 52);
+            this.dgVisualizar.Location = new System.Drawing.Point(64, 78);
+            this.dgVisualizar.MultiSelect = false;
             this.dgVisualizar.Name = "dgVisualizar";
             this.dgVisualizar.ReadOnly = true;
             this.dgVisualizar.RowHeadersVisible = false;
-            this.dgVisualizar.RowHeadersWidth = 51;
             this.dgVisualizar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgVisualizar.Size = new System.Drawing.Size(242, 110);
-            this.dgVisualizar.TabIndex = 41;
-            // 
-            // Nome
-            // 
-            this.Nome.DataPropertyName = "Nome";
-            this.Nome.HeaderText = "Nome";
-            this.Nome.Name = "Nome";
-            this.Nome.ReadOnly = true;
-            this.Nome.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.Nome.Width = 120;
+            this.dgVisualizar.Size = new System.Drawing.Size(242, 202);
+            this.dgVisualizar.TabIndex = 4;
             // 
             // Codigo
             // 
-            this.Codigo.DataPropertyName = "Cod";
             this.Codigo.HeaderText = "Código";
             this.Codigo.Name = "Codigo";
             this.Codigo.ReadOnly = true;
-            this.Codigo.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Codigo.Width = 120;
             // 
-            // btnVisualizarCategoria
+            // Nome
             // 
-            this.btnVisualizarCategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnVisualizarCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVisualizarCategoria.ForeColor = System.Drawing.Color.White;
-            this.btnVisualizarCategoria.Location = new System.Drawing.Point(32, 86);
-            this.btnVisualizarCategoria.Margin = new System.Windows.Forms.Padding(2);
-            this.btnVisualizarCategoria.Name = "btnVisualizarCategoria";
-            this.btnVisualizarCategoria.Size = new System.Drawing.Size(116, 39);
-            this.btnVisualizarCategoria.TabIndex = 42;
-            this.btnVisualizarCategoria.Text = "Visualizar categoria";
-            this.btnVisualizarCategoria.UseVisualStyleBackColor = false;
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            this.Nome.Width = 120;
             // 
-            // btnVisualizarPerfil
+            // btnCategoria
             // 
-            this.btnVisualizarPerfil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btnVisualizarPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVisualizarPerfil.ForeColor = System.Drawing.Color.White;
-            this.btnVisualizarPerfil.Location = new System.Drawing.Point(32, 151);
-            this.btnVisualizarPerfil.Margin = new System.Windows.Forms.Padding(2);
-            this.btnVisualizarPerfil.Name = "btnVisualizarPerfil";
-            this.btnVisualizarPerfil.Size = new System.Drawing.Size(116, 39);
-            this.btnVisualizarPerfil.TabIndex = 43;
-            this.btnVisualizarPerfil.Text = "Visualizar Perfil";
-            this.btnVisualizarPerfil.UseVisualStyleBackColor = false;
+            this.btnCategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategoria.ForeColor = System.Drawing.Color.White;
+            this.btnCategoria.Location = new System.Drawing.Point(37, 12);
+            this.btnCategoria.Name = "btnCategoria";
+            this.btnCategoria.Size = new System.Drawing.Size(127, 44);
+            this.btnCategoria.TabIndex = 5;
+            this.btnCategoria.Text = "Visualizar categoria";
+            this.btnCategoria.UseVisualStyleBackColor = false;
+            this.btnCategoria.Click += new System.EventHandler(this.BtnCategoria_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(341, 154);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(108, 44);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.BtnEditar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrar.Location = new System.Drawing.Point(341, 78);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(108, 44);
+            this.btnCadastrar.TabIndex = 9;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.BtnCadastrar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(341, 236);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(108, 44);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatus.ForeColor = System.Drawing.Color.White;
+            this.btnStatus.Location = new System.Drawing.Point(205, 12);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(127, 44);
+            this.btnStatus.TabIndex = 11;
+            this.btnStatus.Text = "Visualizar status";
+            this.btnStatus.UseVisualStyleBackColor = false;
+            this.btnStatus.Click += new System.EventHandler(this.BtnStatus_Click);
             // 
             // ConfigAdm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(656, 224);
-            this.Controls.Add(this.btnVisualizarPerfil);
-            this.Controls.Add(this.btnVisualizarCategoria);
-            this.Controls.Add(this.dgVisualizar);
+            this.ClientSize = new System.Drawing.Size(483, 308);
+            this.Controls.Add(this.btnStatus);
+            this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.btnCadastrar);
-            this.Controls.Add(this.btnPerfil);
             this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.btnVisualizarStatus);
+            this.Controls.Add(this.btnCategoria);
+            this.Controls.Add(this.dgVisualizar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ConfigAdm";
-            this.Text = "Configuração do administrador";
-            this.Load += new System.EventHandler(this.ConfigAdm_Load);
+            this.Text = "Configurações do administrador";
             ((System.ComponentModel.ISupportInitialize)(this.dgVisualizar)).EndInit();
             this.ResumeLayout(false);
 
@@ -192,14 +176,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnVisualizarStatus;
-        private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnPerfil;
-        private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.DataGridView dgVisualizar;
-        private System.Windows.Forms.Button btnVisualizarCategoria;
-        private System.Windows.Forms.Button btnVisualizarPerfil;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
         private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.Button btnCategoria;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnStatus;
     }
 }
--- GhostBusters_2/GhostBusters_Forms/View/Adm/ConfigAdm.cs
+++ GhostBusters_2/GhostBusters_Forms/View/Adm/ConfigAdm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GhostBusters_Forms.View.Categoria;
+using GhostBusters_Forms.View.Status;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,15 +14,115 @@ namespace GhostBusters_Forms.View.Adm
 {
     public partial class ConfigAdm : Form
     {
+        String operacao = "";
         public ConfigAdm()
         {
             InitializeComponent();
         }
 
-        private void ConfigAdm_Load(object sender, EventArgs e)
+        private void loadDataGrid()
         {
-            dgVisualizar.ReadOnly = true;
-            dgVisualizar.AutoGenerateColumns = false;
+            if (operacao == "Categoria")
+            {
+                //dgVisualizar.DataSource = new Controller().Consultar();
+            }
+            else if (operacao == "Status")
+            {
+                //dgVisualizar.DataSource = new Controller().Consultar();
+            }
+        }
+
+        private void BtnCategoria_Click(object sender, EventArgs e)
+        {
+            operacao = "Categoria";
+        }
+
+        private void BtnStatus_Click(object sender, EventArgs e)
+        {
+            operacao = "Status";
+        }
+
+        private void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (operacao == "Categoria")
+            {
+                var linha = dgVisualizar.CurrentRow.DataBoundItem;
+
+                var menu = new CadastrarCategoria();
+                menu.FormClosed += (x, y) =>
+                {
+                    this.Show();
+                };
+                
+                menu.Show();
+                this.Hide();
+            }
+            else if (operacao == "Status")
+            {
+                var linha = dgVisualizar.CurrentRow.DataBoundItem;
+
+                var menu = new CadastrarStatus();
+                menu.FormClosed += (x, y) =>
+                {
+                    this.Show();
+                };
+
+                menu.Show();
+                this.Hide();
+            }
+            else if (operacao == "")
+            {
+                MessageBox.Show("Escolha o tipo de visualizar e depois clique na operação desejada!");
+            }
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            if (operacao == "Categoria")
+            {
+                var linha = dgVisualizar.CurrentRow.DataBoundItem;
+
+                var menu = new CadastrarCategoria();
+                menu.FormClosed += (x, y) =>
+                {
+                    this.Show();
+                };
+
+                menu.Show();
+                this.Hide();
+            }
+            else if (operacao == "Status")
+            {
+                var linha = dgVisualizar.CurrentRow.DataBoundItem;
+
+                var menu = new CadastrarStatus();
+                menu.FormClosed += (x, y) =>
+                {
+                    this.Show();
+                };
+
+                menu.Show();
+                this.Hide();
+            }
+            else if (operacao == "")
+            {
+                MessageBox.Show("Escolha o tipo de visualizar e depois clique na operação desejada!");
+            }
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            var linha = dgVisualizar.CurrentRow.DataBoundItem;
+
+            if (linha != null)
+            {
+                //new MetodoPagamentoController().Excluir((MetodoPagamento)linha);
+                //loadDataGrid();
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma " + operacao + "!");
+            }
         }
     }
 }
